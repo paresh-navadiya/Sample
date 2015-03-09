bec8a3426a32cfbed60fe82ca26fd289043f0ee7
paresh <navadiya_paresh@hotmail.com>
2015-03-09 08:13:46 -0400

    First commit

---
+++ Sample/Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by ECWIT on 09/03/15.
+//  Copyright (c) 2015 ECWIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by ECWIT on 09/03/15.
+//  Copyright (c) 2015 ECWIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
